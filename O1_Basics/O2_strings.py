d46078ebe2e63e4d1a328f94092d09646b808edd
mohammed akbar <mohammed.akbar1@outlook.com>
2023-02-09 23:59:19 +0530

    Day 1

--- O1_Basics/O2_strings.py
+++ O1_Basics/O2_strings.py
@@ -1,18 +1,29 @@
+# You can use a pair of either single quote or double quote to define strings; you cannot use a mix of them.
+# Unlike other programming languages, there is no difference between single quote and double quote literals.
+# Other ways to include quotes as part of string literal is to use escape character or docstring methods.
+
 print("Today is a good day to learn Python")
 print('Python is fun')
+
 print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
+# If you need to include single quote inside a string, then use double quotes to define the string, vice versa.
+
 print("hello" + "world")
+# String Concatenation using + operator
+
 greeting = "Hello"
-name = "Bruce"
-# name = input("Please enter your name ")
+# name = "Bruce"
+name = input("Please enter your name ")  # get input from user
 print(greeting + name)
+# Note that there is no separator (space) between the two values in the output.
 
 print(greeting + ' ' + name)
 
 age = 24
 print(age)
 
+# type() function returns the datatype of the object that is passed as an argument.
 print(type(greeting))
 print(type(age))
 
--- O1_Basics/O3_EscapeChar.py
+++ O1_Basics/O3_EscapeChar.py
@@ -7,10 +7,11 @@ print(tabbedString)
 print()
 
 print('The pet shop owner said "No, no, \'e \'s uh,...he\'s resting".')
-
 print("The pet shop owner said 'No, no, \'e \'s uh,...he\'s resting'.")
+# In this case, use escape char (\ or backslash) to have quotes (double or single) as part of string.
 
 print("""The pet shop owner said "No, no, \'e \'s uh,...he\'s resting".""")
+# A pair of 3 quotes (single or double) are called as Docstrings.
 
 print()
 
@@ -25,9 +26,12 @@ anotherString = """This string has been \
 split over \
 several \
 lines."""
+# While working with extremely long strings use backslash to indicate python about string continuation.
 
 print(anotherString)
 print()
 
-print("C:\\Users\\timbuchalka\\notes.txt")
-print(r"C:\Users\timbuchalka\notes.txt")
+# How to include backslash character inside a string?
+print("C:\\Users\\timbuchalka\\notes.txt")  # preferred
+# or
+print(r"C:\Users\timbuchalka\notes.txt")  # r => raw strings
