27d5e7ecbd51cf7195ab48c9673cb125f8835aa4
mohammed akbar <mohammed.akbar1@outlook.com>
2023-02-13 22:32:59 +0530

    Day 02

--- O1_Basics/O2_strings.py
+++ O1_Basics/O2_strings.py
@@ -1,6 +1,8 @@
 # You can use a pair of either single quote or double quote to define strings; you cannot use a mix of them.
 # Unlike other programming languages, there is no difference between single quote and double quote literals.
-# Other ways to include quotes as part of string literal is to use escape character or docstring methods.
+# Other ways to include quotes as part of string literal are:
+#       1.  Use escape character or
+#       2.  Docstring.
 
 print("Today is a good day to learn Python")
 print('Python is fun')
@@ -11,12 +13,11 @@ print('We can even include "quotes" in strings')
 
 print("hello" + "world")
 # String Concatenation using + operator
+# Note that there is no separator (space) between the two values in the output.
 
 greeting = "Hello"
 # name = "Bruce"
 name = input("Please enter your name ")  # get input from user
-print(greeting + name)
-# Note that there is no separator (space) between the two values in the output.
 
 print(greeting + ' ' + name)
 
--- O1_Basics/O3_EscapeChar.py
+++ O1_Basics/O3_EscapeChar.py
@@ -7,10 +7,10 @@ print(tabbedString)
 print()
 
 print('The pet shop owner said "No, no, \'e \'s uh,...he\'s resting".')
-print("The pet shop owner said 'No, no, \'e \'s uh,...he\'s resting'.")
+print("The pet shop owner said \"No, no, 'e 's uh,...he's resting\".")
 # In this case, use escape char (\ or backslash) to have quotes (double or single) as part of string.
 
-print("""The pet shop owner said "No, no, \'e \'s uh,...he\'s resting".""")
+print("""The pet shop owner said "No, no, 'e 's uh,...he's resting".""")
 # A pair of 3 quotes (single or double) are called as Docstrings.
 
 print()
@@ -28,6 +28,7 @@ several \
 lines."""
 # While working with extremely long strings use backslash to indicate python about string continuation.
 
+print()
 print(anotherString)
 print()
 
