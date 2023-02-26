71b9e6bd8bbd04ae5d32671635ca3c68eae32b85
mohak85-git <84465568+mohak85-git@users.noreply.github.com>
2023-02-26 13:08:28 +0530

    Delete O2_FlowControl directory

--- O2_FlowControl/O4_truefalse.py
+++
@@ -1,8 +0,0 @@
-day = "Monday"
-temperature = 30
-raining = True
-
-if day == "Saturday" and temperature > 27 and not raining:
-    print("Go swimming")
-else:
-    print("Learn Python")
--- O2_FlowControl/O9_timestables.py
+++
@@ -1,4 +0,0 @@
-for i in range(1, 13):  # nested for loops
-    for j in range(1, 13):
-        print(f"{j} times of {i} is {j * i}")
-    print("-" * 15)
