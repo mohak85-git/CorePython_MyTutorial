369cba9d1403e648c96a9c0fcbd63ff76d50b438
mohak85-git <84465568+mohak85-git@users.noreply.github.com>
2023-02-26 13:02:16 +0530

    Delete O4_Functions_Intro directory

--- O4_Functions_Intro/O11_parameter_types.py
+++
@@ -1,8 +0,0 @@
-def func(p1, p2, *args, k, **kwargs):
-    print(f"positional-or-keyword:...{p1}, {p2}")
-    print(f"var-positional (*args):..{args}")
-    print(f"keyword only:............{k}")
-    print(f"var-keyword:.............{kwargs}")
-
-
-func(1, 2, 3, 4, 5, 9, k=6, key1=7, key2=8)
