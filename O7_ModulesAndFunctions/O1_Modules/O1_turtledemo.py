120c606e5d6fd06840dcc009c5bce7334b281511
“mohak85-git” <mohammed.akbar02@outlook.com>
2023-03-08 20:40:06 +0530

    Modules Done

---
+++ O7_ModulesAndFunctions/O1_Modules/O1_turtledemo.py
@@ -0,0 +1,9 @@
+from turtle import forward, right, circle, done
+
+for i in range(36):
+    print(i)
+    forward(150)
+    right(250)
+    forward(150)
+    circle(105)
+done()
---
+++ O7_ModulesAndFunctions/O1_Modules/O2_example.py
@@ -0,0 +1,15 @@
+# Print all the modules available in python standard library
+# print(dir())
+# for item in dir():
+#     print(item)
+
+# print('*' * 40)
+# # Print all the modules available in builtins module
+# for item in dir(__builtins__):
+#     print(item)
+
+import shelve
+help(shelve)
+
+# import random
+# help(random.randint)
